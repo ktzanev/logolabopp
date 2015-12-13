05078defba992194dfe1d2bb4d9e12ebd803b2df
ktzanev <kroum.tzanev@math.univ-lille1.fr>
2015-12-13 18:54:21 +0100

    changement de taille pour le logo seul de la FRM

--- frm/buildimg.cmd
+++ frm/buildimg.cmd
@@ -1,33 +1,33 @@
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g68x50 -dPDFFitPage -sOutputFile=frm_68_50.png frm.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g136x100 -dPDFFitPage -sOutputFile=frm_136_100.png frm.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g408x300 -dPDFFitPage -sOutputFile=frm_408_300.png frm.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g952x700 -dPDFFitPage -sOutputFile=frm_952_700.png frm.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g70x50 -dPDFFitPage -sOutputFile=frm_70_50.png frm.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g140x100 -dPDFFitPage -sOutputFile=frm_140_100.png frm.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g420x300 -dPDFFitPage -sOutputFile=frm_420_300.png frm.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g980x700 -dPDFFitPage -sOutputFile=frm_980_700.png frm.pdf
 
-gswin32c -sDEVICE=png16m -dNOPAUSE -dBATCH -dSAFER -g3808x2800 -dPDFFitPage -sOutputFile=frm_big.png frm.pdf
+gswin32c -sDEVICE=png16m -dNOPAUSE -dBATCH -dSAFER -g3920x2800 -dPDFFitPage -sOutputFile=frm_big.png frm.pdf
 
-convert -resize 68x50 frm_big.png frm_68_50.jpg
-convert -resize 136x100 frm_big.png frm_136_100.jpg
-convert -resize 408x300 frm_big.png frm_408_300.jpg
-convert -resize 952x700 frm_big.png frm_952_700.jpg
+convert -resize 70x50 frm_big.png frm_70_50.jpg
+convert -resize 140x100 frm_big.png frm_140_100.jpg
+convert -resize 420x300 frm_big.png frm_420_300.jpg
+convert -resize 980x700 frm_big.png frm_980_700.jpg
 
-convert -resize 68x50 frm_big.png frm_68_50.gif
-convert -resize 136x100 frm_big.png frm_136_100.gif
-convert -resize 408x300 frm_big.png frm_408_300.gif
-convert -resize 952x700 frm_big.png frm_952_700.gif
+convert -resize 70x50 frm_big.png frm_70_50.gif
+convert -resize 140x100 frm_big.png frm_140_100.gif
+convert -resize 420x300 frm_big.png frm_420_300.gif
+convert -resize 980x700 frm_big.png frm_980_700.gif
 
 rm frm_big.png
 
 REM -------------------------------------------- BLANC
 
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g68x50 -dPDFFitPage -sOutputFile=frm_blanc_68_50.png frm_blanc.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g136x100 -dPDFFitPage -sOutputFile=frm_blanc_136_100.png frm_blanc.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g408x300 -dPDFFitPage -sOutputFile=frm_blanc_408_300.png frm_blanc.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g952x700 -dPDFFitPage -sOutputFile=frm_blanc_952_700.png frm_blanc.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g70x50 -dPDFFitPage -sOutputFile=frm_blanc_70_50.png frm_blanc.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g140x100 -dPDFFitPage -sOutputFile=frm_blanc_140_100.png frm_blanc.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g420x300 -dPDFFitPage -sOutputFile=frm_blanc_420_300.png frm_blanc.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g980x700 -dPDFFitPage -sOutputFile=frm_blanc_980_700.png frm_blanc.pdf
 
 REM -------------------------------------------- NOIR
 
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g68x50 -dPDFFitPage -sOutputFile=frm_noir_68_50.png frm_noir.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g136x100 -dPDFFitPage -sOutputFile=frm_noir_136_100.png frm_noir.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g408x300 -dPDFFitPage -sOutputFile=frm_noir_408_300.png frm_noir.pdf
-gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g952x700 -dPDFFitPage -sOutputFile=frm_noir_952_700.png frm_noir.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g70x50 -dPDFFitPage -sOutputFile=frm_noir_70_50.png frm_noir.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g140x100 -dPDFFitPage -sOutputFile=frm_noir_140_100.png frm_noir.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g420x300 -dPDFFitPage -sOutputFile=frm_noir_420_300.png frm_noir.pdf
+gswin32c -sDEVICE=pngalpha -dNOPAUSE -dBATCH -dSAFER -g980x700 -dPDFFitPage -sOutputFile=frm_noir_980_700.png frm_noir.pdf
 
